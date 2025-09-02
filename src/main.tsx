e637560b9cd35ad2da8d8add5b9709ac75e70a37
Meerav Shah <meeravshah29@gmail.com>
2025-09-02 12:43:28 -0400

    added vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { inject } from '@vercel/analytics'
+
+// Inject Vercel Web Analytics in production only
+if (import.meta.env.PROD) {
+  inject()
+}
 
 createRoot(document.getElementById("root")!).render(<App />);
